1093736b8518fe2dcb05e0631a8ec87676166238
Eltac Shixseyidov <e.shixseyidov@turanbank.az>
2022-11-05 17:01:52 +0400

    make it better

--- src/main/java/com/fastlogin/falog/service/impl/UrlServiceImpl.java
+++ src/main/java/com/fastlogin/falog/service/impl/UrlServiceImpl.java
@@ -22,7 +22,7 @@ import java.util.NoSuchElementException;
 @Log4j2
 public class UrlServiceImpl implements UrlService {
 
-    private static final String HOST_ADDRESS = "http://localhost:8080/";
+    private static final String HOST_ADDRESS = "https://short-me-api.herokuapp.com/";
 
     private final UrlUtil urlUtil;
 
